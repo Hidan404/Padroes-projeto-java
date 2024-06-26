e169f046c96dd44e7abefcd796e8ff99ebf00f53
Ronald Antonio Oliveira de sousa <ronaldkurouzo@gmail.com>
2024-06-26 10:02:20 -0300

    final

--- src/App.java
+++ src/App.java
@@ -1,3 +1,9 @@
+import Strategy.Comportamento;
+import Strategy.ComportamentoAgressivo;
+import Strategy.ComportamentoDefensivo;
+import Strategy.ComportamentoNormal;
+import Strategy.Robo;
+
 public class App {
     public static void main(String[] args) throws Exception {
 
@@ -12,5 +18,25 @@ public class App {
 
         SyngletonLazyHolder lazyHolder = SyngletonLazyHolder.getInstaciLazy();
         System.out.println(lazyHolder);
+
+
+        //Strategy
+        Comportamento comportamento1 = new ComportamentoNormal();
+        Comportamento comportamento2 = new ComportamentoDefensivo();
+        Comportamento comportamento3 = new ComportamentoAgressivo();
+
+        Robo robo = new Robo();
+
+        robo.setStrategy(comportamento1);
+        robo.mover();
+        robo.setStrategy(comportamento2);
+        robo.mover();
+        robo.setStrategy(comportamento3);
+        robo.mover();
+        
+
+       
+
+
     }
 }
